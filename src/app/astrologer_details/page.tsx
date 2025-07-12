53e9bd825215fa31f208711c055c2ca2ff0325be
Ellchapo <singh.aakash0000@gmail.com>
2025-07-13 02:07:34 +0530

    detail

---
+++ src/app/astrologer_details/page.tsx
@@ -0,0 +1,10 @@
+import AstrologerDetail from '@/components/astrologer_detail/detail'
+import React from 'react'
+
+function Detail() {
+  return (
+    <div><AstrologerDetail/></div>
+  )
+}
+
+export default Detail
